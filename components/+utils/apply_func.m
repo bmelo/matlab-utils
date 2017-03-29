8e9f14610734bc726cdd6ec21038f221a44e8c3e
Bruno Melo <bruno.melo@idor.org>
2017-03-29 13:50:48 -0300

    Fixing function to avoid a list of parameters instead only two (function name and cell of parameters).

--- components/+utils/apply_func.m
+++ components/+utils/apply_func.m
@@ -1,8 +1,8 @@
-function [ output_args ] = apply_func( fhandle, varargin )
+function [ output_args ] = apply_func( fhandle, args )
 %APPLY_FUNC Function to simplify function_handle use
 %   Attempt to facilitate manipulation of parameters passed to function_handles
 
-[output_args] = fhandle(varargin{:});
+[output_args] = fhandle(args{:});
 
 end
 
