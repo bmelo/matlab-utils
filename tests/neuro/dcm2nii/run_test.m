e566fc0f0cad0f801f663dbf546f695bfdba5965
Bruno Melo <bruno.melo@idor.org>
2017-06-07 16:40:35 -0300

    version 0.4.0

--- tests/neuro/dcm2nii/run_test.m
+++
@@ -1,8 +0,0 @@
-% Needs MRICRON 
-% https://www.nitrc.org/frs/?group_id=889
-%
-
-MRICRON_DIR = fullfile( pwd, 'vendors', 'mricron' );
-
-
-
--- tests/relatorioArquivos.m
+++
@@ -1,8 +0,0 @@
-mes = 'Outubro';
-fid = fopen( sprintf('arquivos%s.txt', mes) );
-Dados = textscan(fid,'%s%s%s%s%s%s%f%s%s%s%s', 'Delimiter',' ', 'MultipleDelimsAsOne', true, 'EmptyValue', -Inf);
-fclose(fid);
-
-tamDados = size(Dados);
-tamanho = sum(Dados{7});
-fprintf( '%s: %.2f GB\n', mes, tamanho/1024/1024/1024 );
