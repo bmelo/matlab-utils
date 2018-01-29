30ec6b3ee946dfe2b74336ffddc8f4469b28e612
Bruno Melo <bruno.melo@idor.org>
2018-01-29 14:37:43 -0200

    Including Center Of Gravity (cog) function - [neuro.imgs.cog]

---
+++ components/+neuro/+imgs/cog.m
@@ -0,0 +1,64 @@
+function [ v_cog ] = cog( vol, mask )
+%COG - Center Of Gravity function
+%   Based on COG function present in FSLSTATS
+
+v_cog = [0 0 0];
+val   = 0;
+total = 0;
+vx    = 0;
+vy    = 0;
+vz    = 0;
+tot   = 0;
+n     = 0;
+
+
+% Opening images
+volnii = load_nii(vol);
+
+% Masking, if necessary
+if ~isempty(mask)
+    if ischar(mask)
+        masknii = load_nii(mask);
+    else
+        masknii = mask;
+    end
+    volnii.img(~masknii.img) = NaN;
+end
+
+
+% More settings
+vmin  = min(volnii.img(:));
+
+%nlim  = sqrt(vol.nvoxels());
+%if (nlim<1000)
+%    nlim=1000;
+%end
+lims = utils.dimlims( volnii.img );
+
+for z = lims(3,1):lims(3,2)
+    for y = lims(2,1):lims(2,2)
+        for x = lims(1,1):lims(1,2)
+            n   = n+1;
+            
+            if( isnan(volnii.img(x,y,z)) )
+                continue;
+            end
+            
+            val = volnii.img(x,y,z) - vmin;
+            vx  = vx + val*x;
+            vy  = vy + val*y;
+            vz  = vz + val*z;
+            tot = tot+val;
+            %if (n>nlim)
+            %    total = total + tot; 
+            %    v_cog = ( v_cog + [vx vy vz] );
+            %    n = 0; tot = 0; vx = 0; vy = 0; vz = 0;
+            %end
+        end
+    end
+end
+
+total = total + tot; 
+v_cog = ( v_cog + [vx vy vz] ) / total;
+
+end
---
+++ components/+utils/dimlims.m
@@ -0,0 +1,18 @@
+function [ lims ] = dimlims( data )
+%DIMLIMS - Extract limits with information in a matrix
+% Output: min and max value for each dimension (row).
+
+positions = find( ~isnan(data(:)) );
+n_items = length(positions);
+dims = size(data);
+idxs = zeros(n_items, length(dims));
+pos = cell(1, length(dims));
+
+for k = 1:n_items
+    [pos{:}] = ind2sub(dims, positions(k));
+    idxs(k, :) = [pos{:}];
+end
+
+lims = [min(idxs); max(idxs)]';
+
+end
