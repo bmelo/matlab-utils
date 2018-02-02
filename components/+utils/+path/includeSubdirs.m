1091663fc3a68834b7347b6a24f1e53dfa8d124e
Bruno Melo <bruno.melo@idor.org>
2018-02-02 07:54:39 -0200

    Treating the error by including subdirectories using Command Window.

--- components/+utils/+path/includeSubdirs.m
+++ components/+utils/+path/includeSubdirs.m
@@ -1,7 +1,14 @@
 function includeSubdirs( subdirs )
 
 S = dbstack('-completenames');
-DIR = fileparts(S(2).file);
+% Trying get directory of call function or current
+try
+    DIR = fileparts(S(2).file);
+catch e
+    DIR = pwd();
+end
+
+% Including each subdirectory
 for k=1:length( subdirs )
     utils.path.initFolder( fullfile(DIR, subdirs{k}) );
 end
