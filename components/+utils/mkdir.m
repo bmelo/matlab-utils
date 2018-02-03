ee5ed10edd46d0b24a09a9b965f98d321951424d
Bruno Melo <bruno.melo@idor.org>
2018-02-03 21:45:28 -0200

    Add wrapper of function mkdir that only creates a directory if it is necessary.

---
+++ components/+utils/mkdir.m
@@ -0,0 +1,10 @@
+function [ directory ] = mkdir( directory, varargin )
+%MKDIR Summary of this function goes here
+%   Detailed explanation goes here
+
+if ~isdir(directory)
+    mkdir(directory)
+end
+
+end
+
