c4cb1d6cd7f047b424dcdc2d1cc9071907f4f129
Bruno Melo <bruno.melo@idor.org>
2018-09-06 09:19:44 -0300

    [Enh] Extra parameter to control output of resolve_names.m and get full filename or only basename.

--- components/+utils/resolve_name.m
+++ components/+utils/resolve_name.m
@@ -1,11 +1,13 @@
-function filename = resolve_name(filename)
+function filename = resolve_name(filename, full)
 % RESOLVE_NAME - returns file/folder that can be found by the pattern
 %   returns the first found
 
 import utils.resolve_names;
 
+if nargin < 2, full = true; end
+
 if( ~exist(filename, 'file') )
-    dirFs = resolve_names( filename );
+    dirFs = resolve_names( filename, full );
     if( isempty(dirFs) || ~iscell(dirFs) )
         filename =  [];
     else
--- components/+utils/resolve_names.m
+++ components/+utils/resolve_names.m
@@ -1,8 +1,10 @@
-function out = resolve_names( pattern )
+function out = resolve_names( pattern, full )
 % RESOLVE_NAMES - returns files/folders that can be found by the pattern
 %
 %
 import utils.resolve_names;
+
+if nargin < 2, full = true; end
 
 pts = regexp(pattern, '(?<=\*[^/]*)/(?=.)', 'split');
 baseDir = pts{1};
@@ -20,17 +22,20 @@ for k=1:length(dirFs)
         continue;
     end
     
-    fullFind = fullfile( fileparts(baseDir), dirFs(k).name );
+    fname = dirFs(k).name;
+    if full
+        fname = fullfile( fileparts(baseDir), fname );
+    end
     %% Faz a varredura recursiva ou não
     if length(pts) > 1 && any(baseDir == '*')
-        childFinds = resolve_names( fullfile( fullFind, pts{2:end} ) );
+        childFinds = resolve_names( fullfile( fname, pts{2:end} ) );
         if ~islogical(childFinds) && ~isempty(childFinds) 
             for n = 1:length( childFinds )
                 finds{end+1} = childFinds{n};
             end
         end
     else
-        finds{end+1} = fullFind;
+        finds{end+1} = fname;
     end
 end
 out = finds;
